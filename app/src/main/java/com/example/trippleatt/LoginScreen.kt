397d22f52b71d6bf78f2d11a2a49a9c18079cd67
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-26 03:30:40 +0530

    otp verified on Login screen activity

--- app/src/main/java/com/example/trippleatt/LoginScreen.kt
+++ app/src/main/java/com/example/trippleatt/LoginScreen.kt
@@ -1,11 +1,138 @@
 package com.example.trippleatt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.*
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import java.util.concurrent.TimeUnit
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+
+import android.text.TextUtils
+import android.view.View
+import com.google.android.gms.tasks.TaskExecutors
+
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.PhoneAuthProvider.ForceResendingToken
+import com.google.firebase.auth.PhoneAuthProvider.OnVerificationStateChangedCallbacks
+import com.google.firebase.auth.PhoneAuthCredential
 
 class LoginScreen : AppCompatActivity() {
+
+    private lateinit var btnGenerateOtp: Button
+    private lateinit var etPhoneNumber: EditText
+
+    private var phoneNumber: String = ""
+    private var otp: Int = 0
+
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var mCallbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    private var verificationCode: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViews()
+
+        //StartFirebaseLogin()
+
+        Log.d(TAG, "Login Screen activity created")
+
+        btnGenerateOtp.setOnClickListener {
+            phoneNumber = etPhoneNumber.text.toString().trim()
+
+            phoneNumber = "+91$phoneNumber"
+            Log.d(TAG, phoneNumber)
+
+            startPhoneNumberVerification(phoneNumber)
+
+        }
+
+        mCallbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onCodeSent(verificationId: String, token: ForceResendingToken) {
+                super.onCodeSent(verificationId,token)
+
+                //Log.d(TAG, "onCodeSent:$verificationId")
+
+                // Save verification ID and resending token so we can use them later
+                verificationCode = verificationId
+                Log.d(TAG, "Code sent Successfully.")
+            }
+
+            override fun onVerificationCompleted(
+                credential: PhoneAuthCredential
+            ) {
+                val code = credential.smsCode
+                if (code!=null){
+                    verifyCode(code)
+                }
+                Log.d(TAG, "verification completed:$credential")
+                Toast.makeText(this@LoginScreen, "verification completed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.d(TAG, "onVerificationFailed:$e")
+                Toast.makeText(this@LoginScreen, "onVerificationFailed:$e", Toast.LENGTH_LONG).show()
+
+                if (e is FirebaseAuthInvalidCredentialsException){
+                    Log.d(TAG, "FirebaseAuthInvalidCredentialsException: Invalid request")
+                }
+                else if(e is FirebaseTooManyRequestsException){
+                    Log.d(TAG, "FirebaseTooManyRequestsException: The SMS quota for the project has been exceeded")
+                }
+            }
+
+        }
+
+    }
+
+    fun startPhoneNumberVerification(phoneNumber: String){
+        val options = PhoneAuthOptions.newBuilder(auth)
+            .setPhoneNumber(phoneNumber)
+            .setTimeout(60, TimeUnit.SECONDS)
+            .setActivity(this)
+            .setCallbacks(mCallbacks)
+            .build()
+
+        PhoneAuthProvider.verifyPhoneNumber(options)
+    }
+
+    fun verifyCode(code: String){
+
+        val intent = Intent(this, OtpVerification::class.java)
+        intent.putExtra("code", code)
+        intent.putExtra("verificationCode", verificationCode)
+        startActivity(intent)
+        //below line is used for getting getting credentials from our verification id and code.
+        val credential = PhoneAuthProvider.getCredential(verificationCode, code)
+        //after getting credential we are calling sign in method.
+        //after getting credential we are calling sign in method.
+        //signInWithCredential(credential)
+    }
+
+    fun findViews(){
+        btnGenerateOtp = findViewById(R.id.btnSendOtp)
+
+        etPhoneNumber = findViewById(R.id.etMobileNumber)
+
+        auth = Firebase.auth
+    }
+
+
+
+    companion object {
+        private const val TAG = "PhoneAuthActivity"
     }
 }
--- app/src/main/java/com/example/trippleatt/OtpVerification.kt
+++ app/src/main/java/com/example/trippleatt/OtpVerification.kt
@@ -2,10 +2,87 @@ package com.example.trippleatt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class OtpVerification : AppCompatActivity() {
+
+    private lateinit var code: String
+    private lateinit var verificationCode: String
+
+    private lateinit var etOtpBox1: EditText
+    private lateinit var etOtpBox2: EditText
+    private lateinit var etOtpBox3: EditText
+    private lateinit var etOtpBox4: EditText
+    private lateinit var etOtpBox5: EditText
+    private lateinit var etOtpBox6: EditText
+    private lateinit var btnOtpVerify: Button
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp_verification)
+
+        initViews()
+
+        val intent = intent
+
+        auth = Firebase.auth
+
+        code = intent.getStringExtra("code").toString()
+        verificationCode = intent.getStringExtra("verificationCode").toString()
+
+        etOtpBox1.setText(code[0].toString())
+        etOtpBox2.setText(code[1].toString())
+        etOtpBox3.setText(code[2].toString())
+        etOtpBox4.setText(code[3].toString())
+        etOtpBox5.setText(code[4].toString())
+        etOtpBox6.setText(code[5].toString())
+
+        btnOtpVerify.setOnClickListener {
+            verifyOtp(verificationCode, code)
+        }
+
+
+    }
+
+    fun verifyOtp(verificationCode: String, code: String){
+
+        //below line is used for getting getting credentials from our verification id and code.
+        val credential = PhoneAuthProvider.getCredential(verificationCode, code)
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    val intent  = Intent(this, WelcomeScreen::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this, task.exception!!.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    fun initViews(){
+        etOtpBox1 = findViewById(R.id.otp_edit_box1)
+        etOtpBox2 = findViewById(R.id.otp_edit_box2)
+        etOtpBox3 = findViewById(R.id.otp_edit_box3)
+        etOtpBox4 = findViewById(R.id.otp_edit_box4)
+        etOtpBox5 = findViewById(R.id.otp_edit_box5)
+        etOtpBox6 = findViewById(R.id.otp_edit_box6)
+        btnOtpVerify = findViewById(R.id.btnOtpVerify)
+    }
+
+    companion object {
+        private const val TAG = "PhoneAuthActivity"
     }
 }
