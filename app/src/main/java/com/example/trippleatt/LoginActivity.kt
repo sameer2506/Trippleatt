3e345e541a4a569c6fe0bc4f8c5afd0ade35ad72
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-25 08:46:31 +0530

    login activity design

--- app/src/main/java/com/example/trippleatt/LoginActivity.kt
+++ app/src/main/java/com/example/trippleatt/LoginActivity.kt
@@ -1,14 +1,11 @@
 package com.example.trippleatt
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, LoginActivity::class.java))
+        setContentView(R.layout.activity_login)
     }
 }
