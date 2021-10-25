460d15c0111197ecc518755c6d6f21d15a76b79f
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-25 14:22:10 +0530

    Business Sign Up 3 activity created

---
+++ app/src/main/java/com/example/trippleatt/BusinessSignUp3.kt
@@ -0,0 +1,11 @@
+package com.example.trippleatt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BusinessSignUp3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business_sign_up3)
+    }
+}
