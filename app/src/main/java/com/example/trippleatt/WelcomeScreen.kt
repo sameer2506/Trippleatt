937a4ff21d46a91024cd280db9e2d13f0fdb60e8
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-25 14:36:53 +0530

    welcome screen activity created

---
+++ app/src/main/java/com/example/trippleatt/WelcomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.trippleatt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
