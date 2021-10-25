24e9647a0c9cacc4c8062e00d320d5d47e2d794e
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-25 13:33:37 +0530

    OTP Verification design created

--- app/src/main/java/com/example/trippleatt/OtpVerification.kt
+++ app/src/main/java/com/example/trippleatt/OtpVerification.kt
@@ -1,14 +1,11 @@
 package com.example.trippleatt
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OtpVerification : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, OtpVerification::class.java))
+        setContentView(R.layout.activity_otp_verification)
     }
 }
