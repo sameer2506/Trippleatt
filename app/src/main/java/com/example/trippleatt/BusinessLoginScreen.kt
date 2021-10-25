2e2454380db96c57c0a57ee730283a139a0b629f
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-25 08:57:33 +0530

    busineess login screen created

--- app/src/main/java/com/example/trippleatt/BusinessLoginScreen.kt
+++ app/src/main/java/com/example/trippleatt/BusinessLoginScreen.kt
@@ -3,9 +3,9 @@ package com.example.trippleatt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class BusinessLoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_business_login_screen)
     }
 }
--- app/src/main/java/com/example/trippleatt/LoginScreen.kt
+++ app/src/main/java/com/example/trippleatt/LoginScreen.kt
@@ -1,14 +1,11 @@
 package com.example.trippleatt
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        startActivity(Intent(this, BusinessLoginScreen::class.java))
+        setContentView(R.layout.activity_login)
     }
 }
