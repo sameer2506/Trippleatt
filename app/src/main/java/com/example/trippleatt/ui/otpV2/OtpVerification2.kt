5f5db2fc774269d4b50e642a7da37942c474c86d
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-26 19:50:11 +0530

    otp verification2 activity created

--- app/src/main/java/com/example/trippleatt/ui/otpV2/OtpVerification2.kt
+++ app/src/main/java/com/example/trippleatt/ui/otpV2/OtpVerification2.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trippleatt.R
 
-class OtpVerification : AppCompatActivity() {
+class OtpVerification2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_otp_verification2)
