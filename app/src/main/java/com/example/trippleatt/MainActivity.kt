9fe07f5e16d47c0136ac6263ef89d46218a15dfa
sameer2506 <61904042+sameer2506@users.noreply.github.com>
2021-10-26 19:35:00 +0530

    Business Sign Up 5 activity created

--- app/src/main/java/com/example/trippleatt/MainActivity.kt
+++ app/src/main/java/com/example/trippleatt/MainActivity.kt
@@ -4,12 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trippleatt.ui.LoginScreen
+import com.example.trippleatt.ui.bSU5.BusinessSignUp5
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, LoginScreen::class.java))
+        startActivity(Intent(this, BusinessSignUp5::class.java))
     }
 }
---
+++ app/src/main/java/com/example/trippleatt/ui/bSU5/BusinessSignUp5.kt
@@ -0,0 +1,40 @@
+package com.example.trippleatt.ui.bSU5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trippleatt.R
+import com.example.trippleatt.databinding.ActivityBussinessSignUp5Binding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class BusinessSignUp5 : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityBussinessSignUp5Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityBussinessSignUp5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(com.example.trippleatt.R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
